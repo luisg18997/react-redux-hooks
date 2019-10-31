8a427361b6fa6f754a5fdc7cb4f57ebdbcb480d0
Luis González <lgonzalez@advmsecurity.com>
2019-10-31 16:28:00 -0400

    integrate redux with react hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import {useSelector, useDispatch} from 'react-redux'
+
 function App() {
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Couter : {counter}</h1>
+        <button onClick={()=>{dispatch({type: 'INCREMENT'})}}>INCREMENT</button>
+        <button onClick={()=>{dispatch({type: 'DECREMENT'})}}>DECREMENT</button>
       </header>
     </div>
   );
